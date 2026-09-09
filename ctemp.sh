843042355200b8dedd65918254bed157f36b3d97
brodieliverman07 <brodieliverman07@vt.edu>
2026-09-09 20:57:53 +0000

    script changes and C code

--- ctemp.sh
+++ ctemp.sh
@@ -1,16 +1,21 @@
 #! /bin/bash
 
-if [[ $# -ne 1 ]]; then
-	echo "file name required"
-	exit 1
+if [[ $# -lt 1 ]]; then
+    echo "file name required"
+    exit 1
 fi
 
 dir=$(pwd)
 file="$dir/$1"
 
+if [[ $# -gt 1 && "$1" == "-o" ]]; then
+    file="$dir/$2"
+    trap "vim $file" EXIT
+fi
+
 touch "$file"
 
-echo -e "#include <stdio> \n" >> $file
+echo -e "#include <stdio.h>\n" >> $file
 echo "int main()
 {
     return 0;
